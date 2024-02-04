ad6dbecfa60d2645166a7a3ad1cea9c85e4aeed6
jae9380 <ljy5314@gmail.com>
2024-02-04 22:11:29 +0900

    30강

--- src/main/java/com/ll/sbb20240111/Sbb20240111Application.java
+++ src/main/java/com/ll/sbb20240111/Sbb20240111Application.java
@@ -1,11 +1,14 @@
 package com.ll.sbb20240111;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class Sbb20240111Application {
 
 	public static void main(String[] args) {
